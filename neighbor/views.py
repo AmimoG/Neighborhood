fb44e2bf4c143517f8a2b6101a20bc8d3bfa2315
AmimoG <gilbertmatete6@gmail.com>
2020-10-31 11:21:50 +0300

    Views added settings modifies

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    # posts=Post.objects.all()
+    # if request.method == "POST":
+    #     form = PostForm(request.POST)
+    #     if form.is_valid():
+    #         post = form.save(commit=False)
+    #         post.user = request.user
+    #         post.save()
+    # else:
+    #     form = PostForm()
+
+    # try:
+    #     posts = Post.objects.all()
+    #     print(posts)
+    # except Project.DoesNotExist:
+    #     posts = None
+    return render(request, 'neigbor/index.html',)
+
+def signup(request):
+    global register_form
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignupForm()
+        register_form = {
+            'form': form,
+        }
+    return render(request, 'registration/signup.html', {'form': form})
--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'nirryxif5ax!nimr4*y2!2jfng*giwkv-ck()v!_*6#zt6pt&r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'neighbor',
 ]
 
 MIDDLEWARE = [
