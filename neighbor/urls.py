6e56152e7525c65130faeabce2b4f90dcd27c809
AmimoG <gilbertmatete6@gmail.com>
2020-11-02 20:04:13 +0300

    add files

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('upload/', views.upload, name='upload'),
     path('account/', include('django.contrib.auth.urls')),
-    path('<username>/profile', views.user_profile, name='userprofile'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('<username>/profile', views.user_profile, name='userprofile'),    
     path('profile/', views.profile, name='profile'),
     path('profile/<username>/', views.edit_profile, name='edit_profile'),
 ]
